6407c4a46dc52a36d332478e86fb2898a7fdf6f4
deepakdeedar <ddeedar.cse18@chitkarauniversity.edu.in>
2022-04-30 14:26:31 +0530

    Removed

--- repo_viewer/lib/auth/infrastructure/credentials_storage/credential_storage.dart
+++
@@ -1,7 +0,0 @@
-import 'package:oauth2/oauth2.dart';
-
-abstract class CredentialsStorage {
-  Future<Credentials?> read();
-  Future<void> save(Credentials credentials);
-  Future<void> clear();
-}
